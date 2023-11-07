e1ac444afe52a1ab2218283230a40738d623d66d
muni3066 <44022431+muni3066@users.noreply.github.com>
2023-11-07 12:43:16 +0530

    Add files via upload

---
+++ FizzBuzzServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using FizzBuzzApp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzProgram
+{
+    [TestFixture]
+    public class FizzBuzzServiceTests
+    {
+        private IFizzBuzzService _fizzBuzzService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fizzBuzzService = FizzBuzzService.GetInstance();
+        }
+
+        [TestCase("1", ExpectedResult = "Divided 1 by 3 and divided 1 by 5")]
+        [TestCase("3", ExpectedResult = "Fizz")]
+        [TestCase("5", ExpectedResult = "Buzz")]
+        [TestCase("15", ExpectedResult = "FizzBuzz")]
+        [TestCase("A", ExpectedResult = "Invalid item")]
+        [TestCase("23", ExpectedResult = "Divided 23 by 3 and divided 23 by 5")]
+        public void FizzBuzzService_GetFizzBuzzResult_ReturnsExpectedResult(string input)
+        {
+            _fizzBuzzService.PrintFizzBuzz(input);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace FizzBuzzApp
+{
+    public interface IFizzBuzzService
+    {
+        void PrintFizzBuzz(string n);
+        //string GetFizzBuzzResult(string input);
+    }
+    public class FizzBuzzService : IFizzBuzzService
+    {
+        private static FizzBuzzService _instance;
+
+        private FizzBuzzService() { }
+
+        //Singelton
+        public static FizzBuzzService GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FizzBuzzService();
+            }
+            return _instance;
+        }
+        public void PrintFizzBuzz(string input)
+        {
+            var inputs = input.Split(',');
+            foreach (var item in inputs)
+            {
+                if (int.TryParse(item, out int number))
+                {
+                    if (number % 3 == 0 && number % 5 == 0)
+                    {
+                        //return "FizzBuzz";
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else if (number % 3 == 0)
+                    {
+                        //return "Fizz";
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (number % 5 == 0)
+                    {
+                        //return "Buzz";
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        //return $"Divided {number} by 3 and divided {number} by 5";
+                        Console.WriteLine($"Divided {number} by 3 and divided {number} by 5");
+                    }
+                }
+                else
+                {
+                    //return "Invalid item";
+                    Console.WriteLine("Invalid item");
+                }
+            }
+
+        }
+
+    }
+    //this class separation of responsibilities ensure that each class has a single reason to change.
+    public class FizzBuzzConsole
+    {
+        private readonly IFizzBuzzService _fizzBuzzService;
+
+        public FizzBuzzConsole(IFizzBuzzService fizzBuzzService)
+        {
+            _fizzBuzzService = fizzBuzzService;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Enter comma-separated values:");
+            string input = Console.ReadLine();
+            _fizzBuzzService.PrintFizzBuzz(input);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzzService = FizzBuzzService.GetInstance();
+            var fizzBuzzConsoleUI = new FizzBuzzConsole(fizzBuzzService);
+            fizzBuzzConsoleUI.Run();
+        }
+    }
+}
